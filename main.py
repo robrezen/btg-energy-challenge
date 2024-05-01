f53714e29e12183c1f337d371ec8525034fb980f
Robson Rezende <robson.rezen@outlook.com>
2024-05-01 16:17:54 -0300

    feat: add func search date in file

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import re
-import time
-from functools import wraps
+import os
+import re
+import logging
+import traceback
+from datetime import datetime, date
+from typing import Optional
 
 
 def read_data_file(file_path: str) -> pd.DataFrame:
@@ -25,6 +29,65 @@ def read_contour_file(file_path: str) -> pd.DataFrame:
     header_line = float_raw_lines.pop(0)
     assert len(float_raw_lines) == int(header_line[0])
     return pd.DataFrame(float_raw_lines, columns=['lat', 'long'])
+
+
+def file_date_interpreter(filename: str) -> list:
+    dates = re.search(r'(?<=p)\d*\D\d*', filename)
+    assert dates != None, f'File name {filename} hasn\'t date pattern'
+    return dates.group().split('a')
+
+
+def get_forecast_and_forcasted_date(file: str) -> tuple[date, date]:
+    assert file.endswith('.dat'), f'File {file} is not type .dat'
+    dates: str = file_date_interpreter(file)
+    forecast_date = datetime.strptime(f'{dates[0]}', '%d%m%y').date()
+    forecasted_date = datetime.strptime(f'{dates[1]}', '%d%m%y').date()
+    return forecast_date, forecasted_date
+
+
+def best_forecast_date(f: str, date_searched: date, forecast_date: date, forecasted_date: date,
+                           best_match: Optional[tuple[date, date]], **kwargs) -> bool:
+    if not best_match:
+        return True
+
+    last_forecast_date, last_forecasted_date = best_match
+    last_dates_exist = last_forecast_date is not None and last_forecasted_date is not None
+
+    current_forecast_proximity = abs(date_searched - forecast_date).days
+    current_forecasted_proximity = abs(date_searched - forecasted_date).days
+
+    if last_dates_exist:
+        last_forecast_proximity = abs(date_searched - last_forecast_date).days
+        last_forecasted_proximity = abs(date_searched - last_forecasted_date).days
+    else:
+        return True
+
+    weight_forecast = kwargs['weight_forecast']
+    weight_forecasted = kwargs['weight_forecasted']
+    current_score = (current_forecast_proximity * weight_forecast) + (current_forecasted_proximity * weight_forecasted)
+    last_score = (last_forecast_proximity * weight_forecast) + (last_forecasted_proximity * weight_forecasted)
+
+    return current_score < last_score
+
+
+def search_date_in_file(path: str, date_searched: date) -> str:
+    files = os.listdir(path)
+    assert len(files) > 0, 'Directory is empty'
+    best_match_dates, best_match_file = None, None
+    for f in os.listdir(path):
+        try:
+            forecast_date, forecasted_date = get_forecast_and_forcasted_date(f)
+            is_best_date = best_forecast_date(f, date_searched, forecast_date, forecasted_date, best_match_dates, weight_forecast=0.5, weight_forecasted=0.5)
+            if is_best_date:
+                best_match_dates = (forecast_date, forecasted_date)
+                best_match_file = f
+
+        except Exception as e:
+            logging.warning(f'Error when processing file {f} - Erro {e}')
+            logging.warning(traceback.format_exc())
+
+    assert best_match_file is not None, f'''No files found for date: {date_searched.strftime('%Y-%m-%d')}'''
+    return best_match_file
 
 
 def apply_contour(contour_df: pd.DataFrame, data_df: pd.DataFrame) -> pd.DataFrame:
